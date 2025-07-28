31c3af13fd27c9d59d567d44b7db5bd26b8295d0
devronyrfcl <95331585+devronyrfcl@users.noreply.github.com>
2025-07-28 18:01:30 +0600

    1111111

--- Assets/314 Arts/MarsFPSKit/Scripts/GameLogic/GameModes/PvE/Kit_PvE_SandboxData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace MarsFPSKit
-{
-    public class Kit_PvE_SandboxData : Kit_GameModeNetworkDataBase
-    {
-
-    }
-}
--- Assets/314 Arts/MarsFPSKit/Scripts/Player/Third Person/Kit_DeathCameraBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace MarsFPSKit
-{
-    public abstract class Kit_DeathCameraBase : MonoBehaviour
-    {
-        public abstract void SetupDeathCamera(Kit_ThirdPersonPlayerModel model);
-    }
-}
