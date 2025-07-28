c7e1d0621a08ac5f7de9c7ed6a388c08fe368ff1
devronyrfcl <95331585+devronyrfcl@users.noreply.github.com>
2025-07-28 15:55:55 +0600

    ssd

---
+++ Assets/314 Arts/MarsFPSKit/Scripts/UI/New Main Menu/Options/Kit_OptionBase.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.InputSystem;
+
+namespace MarsFPSKit
+{
+    namespace UI
+    {
+        public enum OptionType { Slider, Dropdown, Toggle, Remap }
+
+        public abstract class Kit_OptionBase : ScriptableObject
+        {
+            /// <summary>
+            /// Returns the name that is displayed
+            /// </summary>
+            /// <returns></returns>
+            public abstract string GetDisplayName();
+
+            /// <summary>
+            /// Returns the hover text
+            /// </summary>
+            /// <returns></returns>
+            public abstract string GetHoverText();
+
+            /// <summary>
+            /// What type is this setting?
+            /// </summary>
+            /// <returns></returns>
+            public abstract OptionType GetOptionType();
+
+            public virtual void OnToggleChange(TextMeshProUGUI txt, bool newValue)
+            {
+
+            }
+
+            public virtual void OnToggleStart(TextMeshProUGUI txt, Toggle toggle)
+            {
+
+            }
+
+            public virtual void OnDropdowChange(TextMeshProUGUI txt, int newValue)
+            {
+
+            }
+
+            public virtual void OnDropdownStart(TextMeshProUGUI txt, TMP_Dropdown dropdown)
+            {
+
+            }
+
+            public virtual void OnSliderChange(TextMeshProUGUI txt, float newValue)
+            {
+
+            }
+
+            public virtual void OnSliderStart(TextMeshProUGUI txt, Slider slider)
+            {
+
+            }
+
+            public virtual void OnRemapStart(Kit_OptionRemap remap)
+            {
+
+            }
+
+            public virtual void OnRemapOpen(Kit_OptionRemap remap)
+            {
+
+            }
+
+
+            public virtual void OnRemapApply(Kit_OptionRemap remap)
+            {
+
+            }
+
+            public virtual void OnRemapChange(Kit_OptionRemap remap)
+            {
+
+            }
+
+            public virtual void OnRemapReset(Kit_OptionRemap remap)
+            {
+
+            }
+        }
+    }
+}
